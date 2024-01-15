a22df3d0f631f8e9d3d118cd7b17aa3571d44d19
rgupta00 <rgupta.mtech@gmail.com>
2024-01-15 16:40:39 +0530

    day 4

---
+++ core/day-4-junit/src/main/java/com/demo/calapp/doubts/Demo.java
@@ -0,0 +1,24 @@
+package com.demo.calapp.doubts;
+//class Tree {
+//    Tree getInstance() {
+//        System.out.println("getInstance of tree");
+//        return new Tree();
+//    }
+//}
+//class Fruit extends Tree {
+//    Object getInstance() {
+//        System.out.println("getInstance of fruit");
+//        return this;
+//    }
+//
+//}
+//
+//class Mango extends Fruit{
+//}
+
+public class Demo {
+    public static void main(String[] args) {
+//        Tree t=new Fruit();
+//        t.getInstance();
+    }
+}
--- core/day-4-junit/src/test/java/com/demo/calapp/CalculatorTest2.java
+++ core/day-4-junit/src/test/java/com/demo/calapp/CalculatorTest2.java
@@ -10,57 +10,26 @@ public class CalculatorTest2 {
 
     private  Calculator calculator;
 
-//    @BeforeAll
-//    public static void beforeAll(){
-//        System.out.println("BeforeAll");
-//    }
-//    @DisplayName("")
     @BeforeEach
     public void before(){
       calculator=new Calculator();
     }
 
-//    @Disabled
-//    @Order(value = 1)
-//    @DisplayName("add test")
-//    @Test
-//    public void addTestAssertAll(){
-//        assertA
-//        ll(
-//                ()-> assertEquals(4, calculator.add(2,1)),
-//                ()-> assertEquals(4, calculator.add(2,1)),
-//                ()-> assertEquals(4, calculator.add(2,1))
-//        );
-//    }
-
-
-//    @Disabled
-//    @Order(value = 1)
-//   @DisplayName("add test")
-//    @Test
-//    public void addTest(){
-//       assertEquals(4, calculator.add(1,3));
-//    }
-
-//    @Order(value = 1)
-//    @DisplayName("add test parameterized")
-//    @ParameterizedTest
-//    @CsvFileSource(resources ="/data.csv",numLinesToSkip = 1)
-//    public void addTest(int n1, int n2, int result){
-//        assertEquals(result, calculator.add(n1, n2));
-//    }
-
     @Order(value = 2)
     @DisplayName("multiply test")
+    @Tag("prod")
     @Test
     public void multiplyTest(){
+        System.out.println("prod test multiplyTest");
         assertEquals(21, calculator.multiply(7,3));
     }
 
     @Order(value = 3)
     @DisplayName("divide with exception")
     @Test
+    @Tag("dev")
     public void divide_with_exception_test(){
+        System.out.println("dev test multiplyTest");
         assertThrows(ArithmeticException.class,
                 ()->calculator.divide(20,0),
                 "divide by zero");
@@ -78,8 +47,5 @@ public class CalculatorTest2 {
         calculator=null;
     }
 
-//    @AfterAll
-//    public static void afterAll(){
-//        System.out.println("AfterAll");
-//    }
+
 }
